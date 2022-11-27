9ca8b325f214703912ef3cdcd2c59d6b5e54c308
Mondinelli <mondinelli@gmail.com>
2022-11-27 20:31:40 +0100

    dsda

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+       <div>
+        halika mi a helyzet?
+       </div>
        
       </header>
     </div>
